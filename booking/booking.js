0ea6bcd3affe4fe5ae0e4a3024ce6518979c57f5
rekavasarhelyinagy <vnreka2000@gmail.com>
2022-12-05 23:02:54 +0100

    monday version

---
+++ booking/booking.js
@@ -0,0 +1,10 @@
+var image = document.getElementById("cart");
+
+btn.addEventListener("click", function () {
+  changeImage();
+});
+
+function changeImage() {
+  image.src = "/booking/cart1.png";
+}
+
